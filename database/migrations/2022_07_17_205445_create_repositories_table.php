d35d8e359475ebf2e6f9fb9bf1b9cacba1051b62
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-07-17 22:53:09 +0100

    Created the repo migration

--- database/migrations/2022_07_17_205445_create_repositories_table.php
+++ database/migrations/2022_07_17_205445_create_repositories_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('repositories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->longText('link')->nullable(false);
+            $table->integer('user_id')->nullable(false);
             $table->timestamps();
         });
     }
