edb3b3f19915233babba9b44747747fc31ddcc81
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-07-19 14:13:15 +0100

    Added iterator and blank state for listing repositories

--- resources/views/repositories.blade.php
+++ resources/views/repositories.blade.php
@@ -228,6 +228,7 @@
                 <!-- This example requires Tailwind CSS v2.0+ -->
                 <div class="pt-5">
                     <ul role="list" class="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                        @forelse(auth()->user()->Repository() as $r)
                         <li class="col-span-1 flex shadow-sm rounded-md">
                             <div class="flex-shrink-0 flex items-center justify-center w-16 bg-pink-600 text-white text-sm font-medium rounded-l-md">GA</div>
                             <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
@@ -246,139 +247,27 @@
                                 </div>
                             </div>
                         </li>
+                            @empty
 
-                        <li class="col-span-1 flex shadow-sm rounded-md">
-                            <div class="flex-shrink-0 flex items-center justify-center w-16 bg-purple-600 text-white text-sm font-medium rounded-l-md">CD</div>
-                            <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
-                                <div class="flex-1 px-4 py-2 text-sm truncate">
-                                    <a href="#" class="text-gray-900 font-medium hover:text-gray-600">Component Design</a>
-                                    <p class="text-gray-500">12 Members</p>
-                                </div>
-                                <div class="flex-shrink-0 pr-2">
-                                    <button type="button" class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                        <span class="sr-only">Open options</span>
-                                        <!-- Heroicon name: solid/dots-vertical -->
-                                        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                            <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
+                            <div class="text-center w-full">
+                                <svg class="mx-auto h-12 w-12 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                    <path vector-effect="non-scaling-stroke" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z" />
+                                </svg>
+                                <h3 class="mt-2 text-sm font-medium text-gray-900">No repositories</h3>
+                                <p class="mt-1 text-sm text-gray-500">Add a repository to this group.</p>
+                                <div class="mt-6">
+                                    <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                        <!-- Heroicon name: solid/plus -->
+                                        <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                            <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />
                                         </svg>
+                                        Add repository
                                     </button>
                                 </div>
                             </div>
-                        </li>
 
-                        <li class="col-span-1 flex shadow-sm rounded-md">
-                            <div class="flex-shrink-0 flex items-center justify-center w-16 bg-yellow-500 text-white text-sm font-medium rounded-l-md">T</div>
-                            <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
-                                <div class="flex-1 px-4 py-2 text-sm truncate">
-                                    <a href="#" class="text-gray-900 font-medium hover:text-gray-600">Templates</a>
-                                    <p class="text-gray-500">16 Members</p>
-                                </div>
-                                <div class="flex-shrink-0 pr-2">
-                                    <button type="button" class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                        <span class="sr-only">Open options</span>
-                                        <!-- Heroicon name: solid/dots-vertical -->
-                                        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                            <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
-                        </li>
+                        @endforelse()
 
-                        <li class="col-span-1 flex shadow-sm rounded-md">
-                            <div class="flex-shrink-0 flex items-center justify-center w-16 bg-green-500 text-white text-sm font-medium rounded-l-md">RC</div>
-                            <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
-                                <div class="flex-1 px-4 py-2 text-sm truncate">
-                                    <a href="#" class="text-gray-900 font-medium hover:text-gray-600">React Components</a>
-                                    <p class="text-gray-500">8 Members</p>
-                                </div>
-                                <div class="flex-shrink-0 pr-2">
-                                    <button type="button" class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                        <span class="sr-only">Open options</span>
-                                        <!-- Heroicon name: solid/dots-vertical -->
-                                        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                            <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
-                        </li>
-
-                        <li class="col-span-1 flex shadow-sm rounded-md">
-                            <div class="flex-shrink-0 flex items-center justify-center w-16 bg-pink-600 text-white text-sm font-medium rounded-l-md">GA</div>
-                            <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
-                                <div class="flex-1 px-4 py-2 text-sm truncate">
-                                    <a href="#" class="text-gray-900 font-medium hover:text-gray-600">Graph API</a>
-                                    <p class="text-gray-500">16 Members</p>
-                                </div>
-                                <div class="flex-shrink-0 pr-2">
-                                    <button type="button" class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                        <span class="sr-only">Open options</span>
-                                        <!-- Heroicon name: solid/dots-vertical -->
-                                        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                            <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
-                        </li>
-
-                        <li class="col-span-1 flex shadow-sm rounded-md">
-                            <div class="flex-shrink-0 flex items-center justify-center w-16 bg-purple-600 text-white text-sm font-medium rounded-l-md">CD</div>
-                            <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
-                                <div class="flex-1 px-4 py-2 text-sm truncate">
-                                    <a href="#" class="text-gray-900 font-medium hover:text-gray-600">Component Design</a>
-                                    <p class="text-gray-500">12 Members</p>
-                                </div>
-                                <div class="flex-shrink-0 pr-2">
-                                    <button type="button" class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                        <span class="sr-only">Open options</span>
-                                        <!-- Heroicon name: solid/dots-vertical -->
-                                        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                            <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
-                        </li>
-
-                        <li class="col-span-1 flex shadow-sm rounded-md">
-                            <div class="flex-shrink-0 flex items-center justify-center w-16 bg-yellow-500 text-white text-sm font-medium rounded-l-md">T</div>
-                            <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
-                                <div class="flex-1 px-4 py-2 text-sm truncate">
-                                    <a href="#" class="text-gray-900 font-medium hover:text-gray-600">Templates</a>
-                                    <p class="text-gray-500">16 Members</p>
-                                </div>
-                                <div class="flex-shrink-0 pr-2">
-                                    <button type="button" class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                        <span class="sr-only">Open options</span>
-                                        <!-- Heroicon name: solid/dots-vertical -->
-                                        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                            <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
-                        </li>
-
-                        <li class="col-span-1 flex shadow-sm rounded-md">
-                            <div class="flex-shrink-0 flex items-center justify-center w-16 bg-green-500 text-white text-sm font-medium rounded-l-md">RC</div>
-                            <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
-                                <div class="flex-1 px-4 py-2 text-sm truncate">
-                                    <a href="#" class="text-gray-900 font-medium hover:text-gray-600">React Components</a>
-                                    <p class="text-gray-500">8 Members</p>
-                                </div>
-                                <div class="flex-shrink-0 pr-2">
-                                    <button type="button" class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                        <span class="sr-only">Open options</span>
-                                        <!-- Heroicon name: solid/dots-vertical -->
-                                        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                            <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
-                        </li>
                     </ul>
                 </div>
                 <!-- /End replace -->
