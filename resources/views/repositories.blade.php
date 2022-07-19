671f5a5d4e9a0d5d5a082f932c629cae88f15d7d
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-07-19 14:04:24 +0100

    Minor tweaks on repositories.blade.php page

--- resources/views/repositories.blade.php
+++ resources/views/repositories.blade.php
@@ -191,7 +191,7 @@
         <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
           <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
         </svg>
-        Publish
+        Add
       </button>
     </span>
 
@@ -446,7 +446,7 @@
                             <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
                                 <div class="flex-1 px-4 py-2 text-sm truncate">
                                     <a href="#" class="text-gray-900 font-medium hover:text-gray-600">Graph API</a>
-                                    <p class="text-gray-500">16 Members</p>
+                                    <p class="text-gray-500">Public repository</p>
                                 </div>
                                 <div class="flex-shrink-0 pr-2">
                                     <button type="button" class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
@@ -467,7 +467,7 @@
                             <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
                                 <div class="flex-1 px-4 py-2 text-sm truncate">
                                     <a href="#" class="text-gray-900 font-medium hover:text-gray-600">Component Design</a>
-                                    <p class="text-gray-500">12 Members</p>
+                                    <p class="text-gray-500">Private repository</p>
                                 </div>
                                 <div class="flex-shrink-0 pr-2">
                                     <button type="button" class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
