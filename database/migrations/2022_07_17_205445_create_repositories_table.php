cf5e2c4aa8e5b7f0f322e8ae8382171c1235a496
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-07-19 21:58:45 +0100

    Updated packages. Did some repository stuff

--- database/migrations/2022_07_17_205445_create_repositories_table.php
+++ database/migrations/2022_07_17_205445_create_repositories_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->longText('link')->nullable(false);
             $table->integer('user_id')->nullable(false);
+            $table->string('vcs')->nullable(false);
             $table->timestamps();
         });
     }
--- resources/views/repositories.blade.php
+++ resources/views/repositories.blade.php
@@ -228,13 +228,27 @@
                 <!-- This example requires Tailwind CSS v2.0+ -->
                 <div class="pt-5">
                     <ul role="list" class="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                        @forelse(auth()->user()->Repository() as $r)
+                        @forelse(auth()->user()->Repository()->get() as $r)
                         <li class="col-span-1 flex shadow-sm rounded-md">
-                            <div class="flex-shrink-0 flex items-center justify-center w-16 bg-pink-600 text-white text-sm font-medium rounded-l-md">GA</div>
+                            <div class="flex-shrink-0 flex items-center justify-center w-16 bg-pink-600 text-white text-sm font-medium rounded-l-md">
+                                @switch($r->vcs)
+                                @case('github')
+                                        <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png">
+                                @break
+
+                                @case('gitlab')
+                                        <img src="https://about.gitlab.com/images/new_logo/A.jpg">
+                                        @break
+
+                                    @default
+                                        <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png">
+                                    @break
+                                @endswitch
+                            </div>
                             <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
                                 <div class="flex-1 px-4 py-2 text-sm truncate">
-                                    <a href="#" class="text-gray-900 font-medium hover:text-gray-600">Graph API</a>
-                                    <p class="text-gray-500">16 Members</p>
+                                    <a href="#" class="text-gray-900 font-medium hover:text-gray-600">{{$r->name}}</a>
+                                    <p class="text-gray-500">Public repository</p>
                                 </div>
                                 <div class="flex-shrink-0 pr-2">
                                     <button type="button" class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
@@ -335,7 +349,7 @@
                             <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
                                 <div class="flex-1 px-4 py-2 text-sm truncate">
                                     <a href="#" class="text-gray-900 font-medium hover:text-gray-600">Graph API</a>
-                                    <p class="text-gray-500">Public repository</p>
+                                    <p class="text-gray-500">PLACEHOLDER</p>
                                 </div>
                                 <div class="flex-shrink-0 pr-2">
                                     <button type="button" class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
@@ -356,7 +370,7 @@
                             <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
                                 <div class="flex-1 px-4 py-2 text-sm truncate">
                                     <a href="#" class="text-gray-900 font-medium hover:text-gray-600">Component Design</a>
-                                    <p class="text-gray-500">Private repository</p>
+                                    <p class="text-gray-500">PLACEHOLDER</p>
                                 </div>
                                 <div class="flex-shrink-0 pr-2">
                                     <button type="button" class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
@@ -372,7 +386,7 @@
 
                     </ul>
                 </div>
-
+            </div>
         </main>
     </div>
 
