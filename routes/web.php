505a5efbb064082d1075106963a264cf92ae1181
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-07-20 19:25:06 +0100

    Added middleware to auth routes and created management page for repositories

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,16 @@ Route::get('/', function () {
     return view('home');
 })->middleware('guest')->name('home');
 
-Route::get('/profile/{username}', [User::class, 'profile'])->name('profile');
+Route::get('/profile/{username}', [User::class, 'profile'])->name('profile')->middleware('auth');
 
-Route::get('/teams', [Teams::class, 'view'])->name('teams');
+Route::get('/teams', [Teams::class, 'view'])->name('teams')->middleware('auth');
 
-Route::get('/issues', [Issues::class, 'view'])->name('issues');
+Route::get('/issues', [Issues::class, 'view'])->name('issues')->middleware('auth');
 
-Route::get('/repositories', [Repositories::class, 'view'])->name('repositories');
+Route::get('/repositories', [Repositories::class, 'view'])->name('repositories')->middleware('auth');
 
-Route::get('/projects', [Projects::class, 'view'])->name('projects');
-Route::get('/projects/{slug}', [Projects::class, 'details'])->name('projects.details');
+Route::get('/projects', [Projects::class, 'view'])->name('projects')->middleware('auth');
+Route::get('/projects/{slug}', [Projects::class, 'details'])->name('projects.details')->middleware('auth');
 
 Route::middleware([
     'auth:sanctum',
