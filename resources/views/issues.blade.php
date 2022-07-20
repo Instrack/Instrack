26f5185e2ea3870f3cf04312d342cc970d415c38
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-07-20 10:57:32 +0100

    Tweaked the content displayed for issues

--- resources/views/issues.blade.php
+++ resources/views/issues.blade.php
@@ -226,28 +226,28 @@
                                         </div>
                                         <div class="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
                                             <div>
-                                                <p class="text-sm font-medium text-indigo-600 truncate">{{$i->User->name}}</p>
+                                                <p class="text-sm font-medium text-indigo-600 truncate">{{$i->title}}</p>
                                                 <p class="mt-2 flex items-center text-sm text-gray-500">
-                                                    <!-- Heroicon name: solid/mail -->
+                                                    {{--<!-- Heroicon name: solid/mail -->
                                                     <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                                         <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                                                         <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                                                    </svg>
-                                                    <span class="truncate">{{$i->User->email}}</span>
+                                                    </svg>--}}
+                                                    <span class="truncate">{{$i->description}}</span>
                                                 </p>
                                             </div>
                                             <div class="hidden md:block">
                                                 <div>
                                                     <p class="text-sm text-gray-900">
                                                         Opened on
-                                                        <time datetime="2020-01-07">{{$i->created_at}}</time>
+                                                        <time datetime="2020-01-07">{{$i->created_at}} by {{$i->User->name}}</time>
                                                     </p>
                                                     <p class="mt-2 flex items-center text-sm text-gray-500">
                                                         <!-- Heroicon name: solid/check-circle -->
                                                         <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                                             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
                                                         </svg>
-                                                        Completed phone screening
+                                                        {{$i->Repository->name}}
                                                     </p>
                                                 </div>
                                             </div>
